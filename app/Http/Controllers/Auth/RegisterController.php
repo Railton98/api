f5a324ecc2fadc1bad88c1cecf010e1ed1fe61fa
Railton's Teck <railtonleal98@gmail.com>
2018-01-21 18:28:38 -0300

    Configuração inicial - Setando o Ambiente

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Confee\Http\Controllers\Auth;
 
-use App\User;
-use App\Http\Controllers\Controller;
+use Confee\User;
+use Confee\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
