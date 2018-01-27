d7338953ad34f102b3274e52d3b0119f40625238
Railton's Teck <railtonleal98@gmail.com>
2018-01-27 18:46:52 -0300

    Repositories part-3

--- app/Support/Domain/Repository/Repository.php
+++ app/Support/Domain/Repository/Repository.php
@@ -4,6 +4,7 @@ namespace Confee\Support\Domain\Repository;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 
 /**
  * Class Repository.
@@ -15,6 +16,42 @@ abstract class Repository
   public function newQuery()
   {
     return app()->make($this->modelClass)->newQuery();
+  }
+
+  public function doQuery($query = null, $take = 15, $paginate = true)
+  {
+    if (!$query) {
+      $query = $this->newQuery();
+    }
+
+    if ($paginate) {
+      return $query->paginate($take);
+    }
+
+    if ($take) {
+      return $query->take($take)->get();
+    }
+
+    return $query->get();
+  }
+
+  public function getAll($take = 15, $paginate = true)
+  {
+    return $this->doQuery(null, $take, $paginate);
+  }
+
+  public function delete(Model $model)
+  {
+    $model->delete();
+
+    return $model;
+  }
+
+  public function deleteCollection(Collection $collection)
+  {
+    $collection->each(function(Model $model) {
+      $this->delete($model);
+    });
   }
 
   /**
